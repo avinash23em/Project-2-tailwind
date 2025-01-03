b6f20f016962056b2361f882ab1c278efea7320c
avinash23em <ap23vi@gmail.com>
2025-01-03 17:33:56 +0530

    intial

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,7 +4,9 @@ import { Video } from "./icon/video";
 
 export function Home(){
     return <div className="w-full">
-<div className=" h-60 bg-black"></div>
+<div className=" h-40 bg-black"></div>
+<p  className="text-1xl text-center pt-8">Monday 14th October</p>
+<p className="text-3xl text-center pr-4 ">Good Morining, Avinash </p>
     <div className="grid grid-cols-10 gap-8 p-8">
     <div className="h-96 rounded-2xl bg-white  md:col-span-3 -translate-y-24 shadow-lg col-span-11 hidden md:block"> 
     <div className="flex flex-col items-center p-8  ">
@@ -24,7 +26,7 @@ export function Home(){
         <div className="col-span-1 pl-2"><Add/>
         <p className="mt-2 text-center">Add</p></div>
         <div className="col-span-2"><Video/>
-        <p className="mt-2 text-center">See recordings</p></div>
+        <p className=" text-center">See recordings</p></div>
         </div>
         
     </div>
